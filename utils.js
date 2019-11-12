f7f1bc9d9169ca98c832b40b53077b0610571a0e
lirugo <roostik21@gmail.com>
2019-11-12 16:28:38 +0200

    M

--- utils.js
+++ utils.js
@@ -8,9 +8,9 @@ class Utils {
                 
         // if(this.isGreenXY(imgData, x, y)) count++;
 
-        // if(this.isGreenXY(imgData, x, y+lengthX)) count++;
+        if(this.isGreenXY(imgData, x, y+lengthY)) count++;
         // if(this.isGreenXY(imgData, x, y-lengthX)) count++;
-        if(this.isGreenXY(imgData, x+lengthY, y)) count++;
+        // if(this.isGreenXY(imgData, x+lengthX, y)) count++;
         // if(this.isGreenXY(imgData, x-lengthX, y)) count++;
         // if(this.isGreenXY(imgData, x-lengthX, y)) count++;
 

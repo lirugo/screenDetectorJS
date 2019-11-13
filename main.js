253e8f8d1a7fb6fb239446fbee804d201b19076e
lirugo <roostik21@gmail.com>
2019-11-13 16:31:43 +0200

    M

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const SEND_FRAME_URL = "http://192.168.88.244:8080/api/web/";
 
 const localVideo = document.getElementById('video');
 const videoStream = new VideoStream(TAG, FPS, SEND_FRAME_URL, localVideo);
-const customCanvas = new CustomCanvas(document.getElementById('canvasMarker'), document.getElementById('canvasForFrame'));
+// const customCanvas = new CustomCanvas(document.getElementById('canvasMarker'), document.getElementById('canvasForFrame'));
 
-customCanvas.setupCanvasMarker();
-customCanvas.drawCanvasMarker();
+// customCanvas.setupCanvasMarker();
+// customCanvas.drawCanvasMarker();
 
 videoStream.setupVideoStream(customCanvas);
 
-console.log(TAG + "Window size " + WIDNWDOW_WIDTH + "x" + WIDNWDOW_HEIGHT)
+// console.log(TAG + "Window size " + WIDNWDOW_WIDTH + "x" + WIDNWDOW_HEIGHT)
 
-Utils.showPreloader("", "hidden");
+// Utils.showPreloader("", "hidden");
 //Update canvas every time in interval
 setInterval(function(){
     videoStream.onEachFrame(customCanvas);

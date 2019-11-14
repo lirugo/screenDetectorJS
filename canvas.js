20eedca3f15c67c16bb400473c603fad8cdbd58a
lirugo <roostik21@gmail.com>
2019-11-14 12:47:01 +0200

    Code cleaning canvasMarker

--- canvas.js
+++ canvas.js
@@ -1,85 +1,67 @@
-class CustomCanvas{
+class CustomCanvas {
 
-    canvasFrameCreated;
+    canvasForFrameCreated;
     canvasMarker;
     canvasFrame;
     ctxMarker;
     ctxFrame;
 
-    constructor(canvasMarker, canvasFrame){
+    constructor(canvasMarker, canvasForFrame) {
         this.canvasMarker = canvasMarker;
-        this.canvasFrame = canvasFrame;
+        this.canvasForFrame = canvasForFrame;
 
         this.ctxMarker = canvasMarker.getContext("2d");
-        this.ctxFrame = canvasFrame.getContext("2d");
-        this.canvasFrameCreated = false;
+        this.ctxFrame = canvasForFrame.getContext("2d");
+
+        this.canvasForFrameCreated = false;
     }
 
-    setupCanvasFrame(width, height){
-        if(!this.canvasFrameCreated){
-            this.canvasFrame.style.width = width;
-            this.canvasFrame.style.height = height;
-            this.canvasFrame.width  = width;
-            this.canvasFrame.height = height;
-
-            console.log(TAG + "Canvas frame size " + width + "x" + height)
-            this.canvasFrameCreated = true;
-        }
-    }
-
-    setupCanvasMarker(){        
-        this.canvasMarker.style.width = WIDNWDOW_WIDTH;
-        this.canvasMarker.style.height = WIDNWDOW_HEIGHT;
+    init() {
         this.canvasMarker.width  = WIDNWDOW_WIDTH;
         this.canvasMarker.height = WIDNWDOW_HEIGHT;
+        this.canvasMarker.style.width = WIDNWDOW_WIDTH;
+        this.canvasMarker.style.height = WIDNWDOW_HEIGHT;
 
-        console.log(TAG + "Canvas marker size " + WIDNWDOW_WIDTH + "x" + WIDNWDOW_HEIGHT)
+        console.log(TAG + `CanvasMarker width: ${this.canvasMarker.width}px, height: ${this.canvasMarker.height}px`);
+            
     }
 
-    drawCanvasMarker(){
-        const greenLengthX = WIDNWDOW_HEIGHT / 14 * 1.5;
-        const greenLengthY = WIDNWDOW_WIDTH / 8 * 1.5;
-        const lengthX = WIDNWDOW_HEIGHT / 14 * 1.8;
-        const lengthY = WIDNWDOW_WIDTH / 8 * 1.8;
-
-
-        let aimTLX = (WIDNWDOW_WIDTH - WIDNWDOW_WIDTH / 1.3);
-        let aimTLY = (WIDNWDOW_HEIGHT - WIDNWDOW_HEIGHT / 1.25);
-        // Draw red aims
-        this.ctxMarker.beginPath();
-        this.ctxMarker.strokeStyle = "#FF0000";
-        this.ctxMarker.lineWidth = 5;
-        this.ctxMarker.rect(aimTLX-lengthX/2, aimTLY-lengthY/2, lengthX, lengthY)
-        this.ctxMarker.stroke();
-        
-
-        //Draw green side markers
+    drawSideMarkers() {
+        let colors = ["#00FF00","#00FF00","#00FF00","#00FF00"];
+        //TODO:: shake colors array
         //Top left
         this.ctxMarker.beginPath();
-        this.ctxMarker.fillStyle = "#00FF00";
-        this.ctxMarker.fillRect(0, 0, greenLengthX, greenLengthY)
+        this.ctxMarker.fillStyle = colors[0];
+        this.ctxMarker.fillRect(WIDNWDOW_WIDTH-greenLengthX, 0, WIDNWDOW_WIDTH, greenLengthY)
         this.ctxMarker.stroke();
         //Top right
         this.ctxMarker.beginPath();
-        this.ctxMarker.fillStyle = "#00FF00";
-        this.ctxMarker.fillRect(WIDNWDOW_WIDTH-greenLengthX, 0, WIDNWDOW_WIDTH, greenLengthY)
+        this.ctxMarker.fillStyle = colors[1];
+        this.ctxMarker.fillRect(0, 0, greenLengthX, greenLengthY)
         this.ctxMarker.stroke();
         //Bottom right
         this.ctxMarker.beginPath();
-        this.ctxMarker.fillStyle = "#00FF00";
-        this.ctxMarker.fillRect(WIDNWDOW_WIDTH-greenLengthX, WIDNWDOW_HEIGHT-greenLengthY, greenLengthX, greenLengthY)
+        this.ctxMarker.fillStyle = colors[2];
+        this.ctxMarker.fillRect(0, WIDNWDOW_HEIGHT-greenLengthY, greenLengthX, greenLengthY)
         this.ctxMarker.stroke();
         //Bottom left
         this.ctxMarker.beginPath();
-        this.ctxMarker.fillStyle = "#00FF00";
-        this.ctxMarker.fillRect(0, WIDNWDOW_HEIGHT-greenLengthY, greenLengthX, greenLengthY)
+        this.ctxMarker.fillStyle = colors[3];
+        this.ctxMarker.fillRect(WIDNWDOW_WIDTH-greenLengthX, WIDNWDOW_HEIGHT-greenLengthY, greenLengthX, greenLengthY)
         this.ctxMarker.stroke();
     }
 
-    fillScreenWithColor(color){
+    drawAims(){
+        const aimTR = [
+            (WIDNWDOW_WIDTH - WIDNWDOW_WIDTH / 1.3),
+            (WIDNWDOW_HEIGHT - WIDNWDOW_HEIGHT / 1.25)
+        ];
+
+        //Top right
         this.ctxMarker.beginPath();
-        this.ctxMarker.rect(0, 0, WIDNWDOW_WIDTH, WIDNWDOW_HEIGHT);
-        this.ctxMarker.fillStyle = color;
-        this.ctxMarker.fill();
+        this.ctxMarker.strokeStyle = "#FF0000";
+        this.ctxMarker.lineWidth = 5;
+        this.ctxMarker.rect(aimTR[0]-lengthX/2, aimTR[1]-lengthY/2, lengthX, lengthY)
+        this.ctxMarker.stroke();
     }
 }
--- video.js
+++ video.js
@@ -2,6 +2,7 @@ class VideoStream {
     FPS;
     URL;
     videoElement;
+    
     videoWidth; videoHeight;
     currentCoreMode;
     skippedFrame;
@@ -13,82 +14,35 @@ class VideoStream {
         this.FPS = FPS;
         this.URL = URL;
         this.videoElement = videoElement;
-        
-        this.MODE_PHONE_DETECTING = 1;
-        this.MODE_SEND_DATA = 2;
-        this.currentCoreMode = this.MODE_PHONE_DETECTING;
-        this.skippedFrame = 0;
+
+        this.videoWidth = 100;
     }
 
-    async setupVideoStream(){
-        
-        let classThis = this;
-        this.videoElement.addEventListener('loadedmetadata', function() {
-            classThis.videoWidth = this.videoWidth;
-            classThis.videoHeight = this.videoHeight;
-            classThis.videoElement.style.width = "100%";
-            classThis.videoElement.style.height = "100%";
-
-            console.log(TAG + `Video stream width: ${classThis.videoWidth}px, height: ${classThis.videoHeight}px`);
-
-            // customCanvas.setupCanvasFrame(this.videoWidth, this.videoHeight);
-
-            // Utils.showPreloader("", "hidden");
-          });
-
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia(
-                {
-                    audio: false, 
-                    video: {
-                        optional: [
-                            {minWidth: 320},
-                            {minWidth: 640},
-                            {minWidth: 1024},
-                            {minWidth: 1280},
-                            {minWidth: 1920},
-                            {minWidth: 2560},
-                        ]
-                    }
-                });
-            console.log(TAG + 'Video stream setup');
-            localVideo.srcObject = stream;
-        } catch (e) {
-            // Utils.showPreloader(`Video stream error: ${e.name}`, "visible");
-            console.log(TAG + `Video stream error: ${e.name}`);
-        }    
-    }
-
-    onEachFrame(customCanvas){
-        if(this.videoWidth && this.videoWidth){
-            let ctxFrame = customCanvas.ctxFrame;
-            ctxFrame.drawImage(video, 0, 0, this.videoWidth, this.videoHeight, 0, 0, this.videoWidth, this.videoHeight);
-
-            if(this.currentCoreMode == this.MODE_PHONE_DETECTING){
-                let imgData = ctxFrame.getImageData(0, 0, this.videoWidth, this.videoHeight);
-
-                let x = (this.videoWidth - this.videoWidth / 1.3);
-                let y = (this.videoHeight - this.videoHeight / 1.25);
-                let aimDetected = Utils.aimDetected(imgData, x, y, this.videoWidth, this.videoHeight);
-
-                // console.log(TAG + isGreen);
-                if(aimDetected){
-                    customCanvas.fillScreenWithColor("white");
-                    this.currentCoreMode = this.MODE_SEND_DATA;
+    init() {
+        let constraints = {
+            video: {
+                    optional: [
+                        {minWidth: 320},
+                        {minWidth: 640},
+                        {minWidth: 1024},
+                        {minWidth: 1280},
+                        {minWidth: 1920},
+                        {minWidth: 2560},
+                    ]
                 }
-            }
+        };
 
-            if(this.currentCoreMode == this.MODE_SEND_DATA){
-                while(this.skippedFrame < 10){
-                    this.skippedFrame++;
-                    return;                    
-                }
-                // Utils.showPreloader("Sending to server...", "visible");
-                console.log(TAG + "SEND TO SERVER")
-                Utils.sendImage2Server(customCanvas.canvasFrame.toDataURL());
-                this.currentCoreMode = "";
-            }
-        }
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(this.handleStreamSuccess)
+            .catch(this.handleStreamError);
     }
 
+    handleStreamSuccess(stream) {
+        window.stream = stream; 
+        video.srcObject = stream;  
+    }
+
+    handleStreamError(error) {
+        console.log(TAG + 'getUserMedia error: ', error);
+    }
 }

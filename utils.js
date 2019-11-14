b8adc051702b1e6f4543d442eb778659bf90d0ba
lirugo <roostik21@gmail.com>
2019-11-14 10:45:02 +0200

    Minor

--- utils.js
+++ utils.js
@@ -123,7 +123,8 @@ class Utils {
             for(let i = 4; i<buildIndex; i++){
                 model += userAgentArray[i] + " ";
             }
-        }else{
+        }
+        if(this.isIOS()){
             model = "iPhone";
         }
 

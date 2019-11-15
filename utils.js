e50f87872b03774aea4537f5b595f32b77e800a4
lirugo <roostik21@gmail.com>
2019-11-15 12:07:35 +0200

    Minor

--- utils.js
+++ utils.js
@@ -56,7 +56,7 @@ class Utils {
         if(this.isGreenXY(imgData, x+halfLengthX/2, y-halfLengthY/2)) count++;
         if(this.isGreenXY(imgData, x-halfLengthX/2, y+halfLengthY/2)) count++;
 
-        return count >= 3;
+        return count >= 2;
     }
 
     static isGreenXY(imgData, x, y) {

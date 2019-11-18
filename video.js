b0efed212ad53c952e6e2631f0a56b555e6a8cdf
lirugo <roostik21@gmail.com>
2019-11-18 11:45:28 +0200

    Test read QR code

--- video.js
+++ video.js
@@ -59,15 +59,15 @@ class VideoStream {
             let imgData = customCanvas.ctxFrame.getImageData(0, 0, this.videoWidth, this.videoHeight);
 
             //Read QR code
-            const code = jsQR(imgData.data, this.videoWidth, this.videoHeight);
-            let qrCode = document.getElementById("QRCode");
+            // const code = jsQR(imgData.data, this.videoWidth, this.videoHeight);
+            // let qrCode = document.getElementById("QRCode");
 
-            if (code) {
-                qrCode.innerText = code.data;
-                console.log("Found QR code", code);
-            }else{
-                qrCode.innerText = "UNKNOWN";
-            }
+            // if (code) {
+            //     qrCode.innerText = code.data;
+            //     console.log("Found QR code", code);
+            // }else{
+            //     qrCode.innerText = "UNKNOWN";
+            // }
 
             if(this.currentCoreMode == this.MODE_PHONE_DETECTING){
                 let aimTL = [
